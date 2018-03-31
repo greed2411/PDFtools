87a49c159f074b89d7e0276b4bdb1cfc4a08a11b
Jaivarsan <jaiimmortal@gmail.com>
2018-03-31 13:01:56 +0530

    support for both jpg and png with filename

--- images2pdfgenerator.py
+++ images2pdfgenerator.py
@@ -1,6 +1,10 @@
 import os
 import img2pdf
 
-# takes all the images in the current working directory and puts as one centered image per page
-with open("output.pdf", "wb") as f:
-    f.write(img2pdf.convert([i for i in os.listdir(os.getcwd()) if i.endswith(".jpg")]))
+filename = input("Enter output filename: ")
+if '.pdf' not in filename: filename = filename + '.pdf'
+
+with open(filename, "wb") as f:
+    f.write(img2pdf.convert([i for i in os.listdir(os.getcwd()) if i.endswith(".png") or i.endswith(".jpg")]))
+
+print("Done. ")
